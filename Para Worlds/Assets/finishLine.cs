82840e76421188e07e0ac7ec161c03bf0e8ae86b
Jesse <1016739@student.sae.edu.au>
2019-09-10 22:05:27 +1000

    YEET LORD
    New Shit just got made

---
+++ Para Worlds/Assets/finishLine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class finishLine : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("Test Level");
+            Debug.Log("Dis Bitch Changing. YEET!");
+        }
+    }
+}
