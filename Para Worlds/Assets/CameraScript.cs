2b21be35da99a0f9c4ade2baf6744194af9be445
Jesse <1016739@student.sae.edu.au>
2019-09-10 21:13:45 +1000

    Camera Fuckery

--- Para Worlds/Assets/CameraScript.cs
+++ Para Worlds/Assets/CameraScript.cs
@@ -15,7 +15,7 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
+        transform.position = new Vector3(transform.position.x, player.transform.position.y + 6, player.transform.position.z);
 
         transform.LookAt(player.transform);
 
